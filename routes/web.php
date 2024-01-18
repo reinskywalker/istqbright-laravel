55350cb3a57cbc24529700f08e53abbff55f62ff
Andre Reynaldi Lusikooy <andre.reynaldi@ai.astra.co.id>
2024-01-18 20:54:05 +0700

    composer update

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+// Default welcome route
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Authenticated user dashboard route
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+// Admin-specific routes under the 'admin' prefix
+Route::middleware(['role:admin'])->prefix('admin')->group(function () {
+    Route::get('/adminhome', [AdminController::class, 'adminhome'])->name('adminhome');
+});
